ccead81ab18112678473f227f8737b27332c79c9
bamdyl <badylcookiestyle@gmail.com>
2021-12-27 00:19:44 +0100

    v0.0.1 Changes : - I've just made basic layout   ~Filters Activity   ~Upload Activity   ~CardView

---
+++ app/src/main/java/com/example/secretary_mobile/FiltersActivity.kt
@@ -0,0 +1,11 @@
+package com.example.secretary_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FiltersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filters)
+    }
+}
