57d1ab631c213d5160621eb25e9590f2439ff39d
bamdyl <badylcookiestyle@gmail.com>
2021-12-27 14:14:09 +0100

    v0.0.3 Changes : - I've just implemented Adapter class and ViewModel class (I need these classes to implement ryclerView)

---
+++ app/src/main/java/com/example/secretary_mobile/Adapter.kt
@@ -0,0 +1,56 @@
+package com.example.secretary_mobile
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter(private val mList: List<ItemsViewModel>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val ItemsViewModel = mList[position]
+
+        holder.textViewId.text = "id: "+ItemsViewModel.id
+        holder.textViewName.text ="name: "+ItemsViewModel.firstName
+        holder.textViewSecondName.text ="second name: "+ItemsViewModel.secondName
+        holder.textViewLastName.text  ="last name: "+ItemsViewModel.lastName
+        holder.textViewMaidenName.text  = "maiden name: "+ItemsViewModel.maidenName
+        holder.textViewFathersName.text  = "fathers name: "+ItemsViewModel.fathersName
+        holder.textViewMothersName.text  = "mothers name: "+ItemsViewModel.mothersName
+        holder.textViewBirthDateName.text = "birthdate: "+ItemsViewModel.birthDate
+        holder.textViewPesel.text ="pesel: "+ItemsViewModel.pesel
+        holder.textViewGender.text  ="gender: "+ ItemsViewModel.gender
+        holder.textViewAdditionalField1.text =ItemsViewModel.additionalField1
+        holder.textViewAdditionalField2.text  =ItemsViewModel.additionalField2
+        holder.textViewAdditionalField3.text  = ItemsViewModel.additionalField3
+        holder.textViewAdditionalField4.text  = ItemsViewModel.additionalField4
+
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val textViewId: TextView = itemView.findViewById(R.id.textViewId)
+        val textViewName: TextView = itemView.findViewById(R.id.textViewName)
+        val textViewSecondName: TextView = itemView.findViewById(R.id.textViewSecondName)
+        val textViewLastName: TextView = itemView.findViewById(R.id.textViewLastName)
+        val textViewMaidenName: TextView = itemView.findViewById(R.id.textViewMaidenName)
+        val textViewFathersName: TextView = itemView.findViewById(R.id.textViewFathersName)
+        val textViewMothersName: TextView = itemView.findViewById(R.id.textViewMothersName)
+        val textViewBirthDateName: TextView = itemView.findViewById(R.id.textViewBirthdate)
+        val textViewPesel: TextView = itemView.findViewById(R.id.textViewPesel)
+        val textViewGender: TextView = itemView.findViewById(R.id.textViewGender)
+        val textViewAdditionalField1: TextView = itemView.findViewById(R.id.textViewAdditionalField1)
+        val textViewAdditionalField2: TextView = itemView.findViewById(R.id.textViewAdditionalField2)
+        val textViewAdditionalField3: TextView = itemView.findViewById(R.id.textViewAdditionalField3)
+        val textViewAdditionalField4: TextView = itemView.findViewById(R.id.textViewAdditionalField4)
+    }
+}
---
+++ app/src/main/java/com/example/secretary_mobile/ItemsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.secretary_mobile
+
+data class ItemsViewModel(
+    val id: String,val firstName: String, val secondName: String, val lastName: String,
+    val maidenName: String, val fathersName: String, val mothersName: String, val birthDate: String,
+    val pesel: String, val imagePath: String, val gender: String, val additionalField1:String,
+    val additionalField2:String,val additionalField3:String,val additionalField4:String) {
+}
